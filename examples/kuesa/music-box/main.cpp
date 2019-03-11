dd385e72e54eda8b8ae4eea45a75892644f858ab
Mauro Persano <mauro.persano@kdab.com>
2019-03-11 19:36:12 -0300

    Introduce music box demo
    Change-Id: I4ec596f64e3ac3d1fcf064833f693d9fe7005bb3

---
+++ examples/kuesa/music-box/main.cpp
@@ -0,0 +1,45 @@
+#include <QGuiApplication>
+#include <QQuickView>
+#include <QResource>
+
+namespace {
+bool initializeAssetResources(const QVector<QString> &fileNames)
+{
+    bool b = true;
+    for (const auto &fileName : fileNames) {
+        b &= QResource::registerResource(fileName);
+        if (!b)
+            qDebug() << "Failed to load assets from" << fileName;
+    }
+    return b;
+}
+}
+
+int main(int argc, char *argv[])
+{
+    {
+        // Set OpenGL requirements
+        QSurfaceFormat format = QSurfaceFormat::defaultFormat();
+#ifndef QT_OPENGL_ES_2
+        format.setVersion(4, 1);
+        format.setProfile(QSurfaceFormat::CoreProfile);
+#else
+        format.setVersion(3, 0);
+        format.setProfile(QSurfaceFormat::NoProfile);
+        format.setRenderableType(QSurfaceFormat::OpenGLES);
+#endif
+        QSurfaceFormat::setDefaultFormat(format);
+    }
+
+    QGuiApplication app(argc, argv);
+
+    initializeAssetResources({QStringLiteral("music-box.qrb"), QStringLiteral("envmap-pink-sunrise.qrb")});
+
+    QQuickView view;
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+    view.setSource(QUrl(QStringLiteral("qrc:/main.qml")));
+    view.resize(960, 540);
+    view.show();
+
+    return app.exec();
+}
