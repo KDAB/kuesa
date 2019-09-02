4267f2674ee8af5b986d9e3f486d8ed3c2316b4e
Jean-Michaël Celerier <jean-michael.celerier@kdab.com>
2019-09-02 17:39:06 +0200

    Add a pure C++ example
    Change-Id: I56c385ed91524737017efec8e4df2fe56e11f61d
    Reviewed-on: https://kuesa-codereview.kdab.com/c/kuesa/kuesa/+/286
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

---
+++ examples/kuesa/manyducks/main.cpp
@@ -0,0 +1,271 @@
+/*
+    main.cpp
+
+    This file is part of Kuesa.
+
+    Copyright (C) 2018-2019 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+    Author: Jean-Michaël Celerier <jean-michael.celerier@kdab.com>
+
+    Licensees holding valid proprietary KDAB Kuesa licenses may use this file in
+    accordance with the Kuesa Enterprise License Agreement provided with the Software in the
+    LICENSE.KUESA.ENTERPRISE file.
+
+    Contact info@kdab.com if any conditions of this licensing are not clear to you.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// Kuesa libraries
+#include <ForwardRenderer>
+#include <SceneEntity>
+#include <DepthOfFieldEffect>
+#include <GLTF2Importer>
+#include <Skybox>
+#include <QScreen>
+// Qt3D
+#include <Qt3DCore/QEntity>
+#include <Qt3DCore/QTransform>
+#include <Qt3DRender/QEnvironmentLight>
+#include <Qt3DRender/QCamera>
+#include <Qt3DExtras/Qt3DWindow>
+#include <Qt3DExtras/QOrbitCameraController>
+
+// Qt & std
+#include <QGuiApplication>
+#include <QResource>
+#include <QDir>
+#include <QTimer>
+#include <array>
+#ifdef Q_OS_ANDROID
+#include <QOpenGLContext>
+#endif
+
+namespace {
+
+// Helper to load .qrb files, used for large environment maps
+bool initializeAssetResources(const QVector<QString> &fileNames)
+{
+#ifdef Q_OS_MACOS
+    QDir resourceDir(qApp->applicationDirPath() + QStringLiteral("/../Resources"));
+#elif defined(Q_OS_IOS)
+    QDir resourceDir(qApp->applicationDirPath() + QStringLiteral("/Library/Application Support"));
+#else
+    QDir resourceDir(qApp->applicationDirPath() + QStringLiteral("/resources"));
+#endif
+    bool b = true;
+    for (const auto &fileName : fileNames) {
+        auto f = resourceDir.path() + QDir::separator() + fileName;
+        b &= QResource::registerResource(f);
+        if (!b)
+            qDebug() << "Failed to load assets from" << f;
+    }
+    return b;
+}
+
+static QString envmap(QString name)
+{
+    return QStringLiteral("qrc:///pink_sunrise_%1%2")
+    #if defined(__APPLE__)
+            .arg("_16f")
+    #else
+            .arg("")
+    #endif
+            .arg(name);
+}
+
+} // namespace
+
+class DefaultEnvMap : public Qt3DRender::QEnvironmentLight
+{
+public:
+    DefaultEnvMap(Qt3DCore::QNode *parent = nullptr):
+        Qt3DRender::QEnvironmentLight{parent}
+    {
+        auto tli = new Qt3DRender::QTextureLoader(this);
+        tli->setSource(QUrl(envmap("irradiance.dds")));
+        tli->setMinificationFilter(Qt3DRender::QAbstractTexture::LinearMipMapLinear);
+        tli->setMagnificationFilter(Qt3DRender::QAbstractTexture::Linear);
+        tli->wrapMode()->setX(Qt3DRender::QTextureWrapMode::ClampToEdge);
+        tli->wrapMode()->setY(Qt3DRender::QTextureWrapMode::ClampToEdge);
+        tli->setGenerateMipMaps(0);
+        setIrradiance(tli);
+
+        auto tls = new Qt3DRender::QTextureLoader(this);
+        tli->setSource(QUrl(envmap("specular.dds")));
+        tls->setMinificationFilter(Qt3DRender::QAbstractTexture::LinearMipMapLinear);
+        tls->setMagnificationFilter(Qt3DRender::QAbstractTexture::Linear);
+        tls->wrapMode()->setX(Qt3DRender::QTextureWrapMode::ClampToEdge);
+        tls->wrapMode()->setY(Qt3DRender::QTextureWrapMode::ClampToEdge);
+        tls->setGenerateMipMaps(0);
+        setSpecular(tls);
+    }
+};
+
+class Window : public Qt3DExtras::Qt3DWindow
+{
+public:
+    static constexpr int Ducks = 50;
+    static constexpr int r = 2000;
+
+    Window()
+    {
+        // Default set-up - here we create an empty entity,
+        // which will contain one scene per glTF object.
+        m_scene = new Kuesa::SceneEntity();
+        m_scene->addComponent(new DefaultEnvMap(m_scene));
+
+        auto rootEntity = m_scene;
+
+        camera()->setPosition(QVector3D(5, 1.5, 5));
+        camera()->setViewCenter(QVector3D(0, 0.5, 0));
+        camera()->setUpVector(QVector3D(0, 1, 0));
+        camera()->setAspectRatio(16. / 9.);
+
+        auto camController = new Qt3DExtras::QOrbitCameraController(m_scene);
+        camController->setCamera(camera());
+
+        auto fg = new Kuesa::ForwardRenderer();
+        fg->setCamera(camera());
+        fg->setGamma(2.2);
+        fg->setExposure(1.);
+        fg->setClearColor("white");
+        setActiveFrameGraph(fg);
+
+        // Load a few glTF models
+        QVector<Kuesa::SceneEntity*> scenes;
+        QVector<Kuesa::GLTF2Importer*> importers;
+        auto gltfImporter = new Kuesa::GLTF2Importer(m_scene);
+        gltfImporter->setSceneEntity(m_scene);
+        gltfImporter->setSource(QUrl{"qrc:///assets/models/duck/Duck.glb"});
+
+        connect(gltfImporter, &Kuesa::GLTF2Importer::statusChanged,
+                this, &Window::on_sceneLoaded);
+
+        // Skybox creation
+        auto skybox = new Kuesa::Skybox(m_scene);
+        skybox->setBaseName(envmap("radiance"));
+        skybox->setExtension(".dds");
+
+        // Depth-of-field
+        auto dof = new Kuesa::DepthOfFieldEffect;
+        dof->setRadius(21.);
+        dof->setFocusRange(3.1);
+        dof->setFocusDistance(6.6);
+        fg->addPostProcessingEffect(dof);
+
+        setRootEntity(m_scene);
+    }
+
+private:
+    void on_sceneLoaded(Kuesa::GLTF2Importer::Status status)
+    {
+        if(status == Kuesa::GLTF2Importer::Ready)
+        {
+            // First let's take the components that we are going to use to create our clones
+            // Gotten from gammaray
+            auto parent = m_scene->entity("KuesaEntity_0");
+
+            auto orig_entity = m_scene->entity("KuesaEntity_2")->childNodes()[1];
+            auto orig_geometry = dynamic_cast<Qt3DCore::QComponent*>(orig_entity->childNodes()[0]);
+            auto orig_material = dynamic_cast<Qt3DCore::QComponent*>(orig_entity->childNodes()[1]);
+
+            // Then create clones by giving them a custom transform, and the same components than before
+            for(int i = 0; i < Ducks; i++)
+            {
+                auto new_entity = new Qt3DCore::QEntity{parent};
+                auto new_transform = new Qt3DCore::QTransform;
+                new_transform->setScale(0.2);
+                new_transform->setTranslation(QVector3D(rand()%r - r/2,rand()%r- r/2,rand()%r - r/2));
+
+                new_transform->setRotationX(rand() % 360);
+                new_transform->setRotationY(rand() % 360);
+                new_transform->setRotationZ(rand() % 360);
+
+                new_entity->addComponent(new_transform);
+                new_entity->addComponent(orig_geometry);
+                new_entity->addComponent(orig_material);
+
+                m_transforms[i] = new_transform;
+            }
+
+            // Animate everyone
+            qreal ms = 1000. / this->screen()->refreshRate();
+            startTimer(ms);
+        }
+    }
+
+    void timerEvent(QTimerEvent *event) override
+    {
+        for(auto transform : m_transforms)
+        {
+            transform->setRotationX(transform->rotationX() + 0.1);
+            transform->setRotationY(transform->rotationY() + 0.1);
+            transform->setRotationZ(transform->rotationZ() + 0.1);
+        }
+    }
+
+    Kuesa::SceneEntity* m_scene{};
+    std::array<Qt3DCore::QTransform*, Ducks> m_transforms;
+};
+
+int main(int argc, char *argv[])
+{
+    bool isES2 = false;
+    {
+        // Set OpenGL requirements
+        QSurfaceFormat format = QSurfaceFormat::defaultFormat();
+#ifndef QT_OPENGL_ES_2
+        format.setVersion(4, 1);
+        format.setProfile(QSurfaceFormat::CoreProfile);
+        format.setSamples(4);
+#else
+#ifndef QT_OPENGL_ES_3
+        isES2 = true;
+#endif
+        format.setVersion(3, 0);
+        format.setProfile(QSurfaceFormat::NoProfile);
+        format.setRenderableType(QSurfaceFormat::OpenGLES);
+#endif
+        QSurfaceFormat::setDefaultFormat(format);
+    }
+
+    QGuiApplication app(argc, argv);
+
+    initializeAssetResources({ QStringLiteral("Duck.qrb") });
+#if defined(Q_OS_IOS) || defined(Q_OS_MACOS) || defined(Q_OS_ANDROID)
+    initializeAssetResources({ QStringLiteral("envmap-pink-sunrise-16f.qrb") });
+#else
+    initializeAssetResources({ QStringLiteral("envmap-pink-sunrise.qrb") });
+#endif
+
+#ifdef Q_OS_ANDROID
+    const QString assetsPrefix = QStringLiteral("assets:/");
+    // Qt builds for android may not define QT_OPENGL_ES_3
+    // Therefore we need a runtime check to see whether we can use ES 3.0 or not
+    QOpenGLContext ctx;
+    ctx.setFormat(QSurfaceFormat::defaultFormat());
+    if (ctx.create()) {
+        const QSurfaceFormat androidFormat = ctx.format();
+        isES2 = (androidFormat.majorVersion() == 2);
+    }
+#else
+    const QString assetsPrefix = QStringLiteral("qrc:/");
+#endif
+
+    Window window;
+
+    window.show();
+
+    return app.exec();
+}
---
+++ examples/kuesa/manyducks/manyducks.pro
@@ -0,0 +1,113 @@
+# manyducks.pro
+#
+# This file is part of Kuesa.
+#
+# Copyright (C) 2018-2019 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+# Author: Jean-Michaël Celerier <jean-michael.celerier@kdab.com>
+#
+# Licensees holding valid proprietary KDAB Kuesa licenses may use this file in
+# accordance with the Kuesa Enterprise License Agreement provided with the Software in the
+# LICENSE.KUESA.ENTERPRISE file.
+#
+# Contact info@kdab.com if any conditions of this licensing are not clear to you.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+TEMPLATE = app
+
+TARGET = manyducks
+
+QT += 3dcore 3drender 3dinput 3dextras kuesa
+SOURCES += \
+    main.cpp
+
+macos: APP_PWD=$$OUT_PWD/$${TARGET}.app/Contents
+else: APP_PWD=$$OUT_PWD
+RES_PWD=$$APP_PWD/resources
+
+target.path = $$[QT_INSTALL_EXAMPLES]/kuesa/$$TARGET
+INSTALLS += target
+
+include($$KUESA_ROOT/kuesa-global.pri)
+
+android {
+    envmaps_dir = ../assets/envmaps
+    envmaps.files = \
+        $${envmaps_dir}/pink_sunrise/pink_sunrise_16f_irradiance.dds \
+        $${envmaps_dir}/pink_sunrise/pink_sunrise_16f_radiance.dds \
+        $${envmaps_dir}/pink_sunrise/pink_sunrise_16f_specular.dds \
+        $${envmaps_dir}/pink_sunrise/pink_sunrise_skybox.dds
+
+    model_dir = ../assets/models/duck
+    model.files = \
+        $${model_dir}/Duck.glb
+
+    envmaps.path = /assets
+    model.path = /assets
+
+    ANDROID_PACKAGE_SOURCE_DIR = $$PWD/android
+    DISTFILES += android/AndroidManifest.xml
+    INSTALLS += envmaps model
+} else {
+    RCC_BINARY_SOURCES += \
+        ../assets/models/duck/Duck.qrc
+
+    ios|macos {
+        RCC_BINARY_SOURCES += \
+            ../assets/envmaps/pink_sunrise/envmap-pink-sunrise-16f.qrc
+    } else {
+        RCC_BINARY_SOURCES += \
+            ../assets/envmaps/pink_sunrise/envmap-pink-sunrise.qrc
+    }
+
+    asset_builder.commands = $$[QT_HOST_BINS]/rcc -binary ${QMAKE_FILE_IN} -o ${QMAKE_FILE_OUT} -no-compress
+    asset_builder.depend_command = $$[QT_HOST_BINS]/rcc -list $$QMAKE_RESOURCE_FLAGS ${QMAKE_FILE_IN}
+    asset_builder.input = RCC_BINARY_SOURCES
+    asset_builder.output = $$RES_PWD/${QMAKE_FILE_IN_BASE}.qrb
+    asset_builder.CONFIG += no_link target_predeps
+    QMAKE_EXTRA_COMPILERS += asset_builder
+
+    windows {
+        RC_ICONS = ../../../resources/kuesa.ico
+        DESTDIR = $$KUESA_BUILD_ROOT/examples/kuesa/$$TARGET
+    } else:ios {
+        envmaps_dir = ../assets/envmaps
+        resfiles.files = \
+            $$RES_PWD/Duck.qrb \
+            $$RES_PWD/envmap-pink-sunrise-16f.qrb
+        resfiles.path = "/Library/Application Support"
+
+        QMAKE_BUNDLE_DATA += resfiles
+
+        OTHER_FILES += Info-ios.plist
+        QMAKE_INFO_PLIST = Info-ios.plist
+
+        OBJECTIVE_SOURCES += ios/iosutils.mm
+        QMAKE_ASSET_CATALOGS += ios/Images.xcassets
+    } else:macos {
+        ICON = ../../../resources/kuesa.icns
+        OTHER_FILES += Info-macos.plist
+        QMAKE_INFO_PLIST = Info-macos.plist
+    }
+}
+
+packagesExist(portaudio-2.0) {
+    CONFIG += link_pkgconfig
+    PKGCONFIG += portaudio-2.0
+    DEFINES += KUESA_HAS_AUDIO=1
+}
+
+OTHER_FILES += doc/src/* \
+    ../assets/models/duck/Duck.qrc \
+    ../assets/envmaps/pink_sunrise/envmap-pink-sunrise.qrc
