9f9c708843cf298bb12058e17c413d8ae242950f
Paul Lemire <paul.lemire@kdab.com>
2020-09-16 13:31:07 +0200

    msaafboresolver_p.h: add missing private API header warning
    Change-Id: Iac18f0a59430a98382023c5143293c4a66d68b01
    Reviewed-on: https://codereview.kdab.com/c/kuesa/kuesa/+/84930
    Reviewed-by: Juan Casafranca <juan.casafranca@kdab.com>

--- src/core/framegraphes/msaafboresolver_p.h
+++ src/core/framegraphes/msaafboresolver_p.h
@@ -29,6 +29,17 @@
 #ifndef KUESA_MSAAFBORESOLVER_P_H
 #define KUESA_MSAAFBORESOLVER_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Kuesa API.  It exists for the convenience
+// of other Kuesa classes.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <Kuesa/private/kuesa_global_p.h>
 #include <Qt3DRender/qrendertargetselector.h>
 
