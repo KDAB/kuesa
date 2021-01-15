c7210675974cbab4d4bc6024d9250888cce66eff
Paul Lemire <paul.lemire@kdab.com>
2021-01-15 11:48:36 +0100

    Update many-ducks to use MeshInstantiator
    Change-Id: I14950686b3cba0ebfdf5dc339922cddaf6d42612
    Reviewed-on: https://codereview.kdab.com/c/kuesa/kuesa/+/91152
    Reviewed-by: Mike Krus <mike.krus@kdab.com>

--- examples/kuesa/manyducks/main.cpp
+++ examples/kuesa/manyducks/main.cpp
@@ -33,6 +33,7 @@
 #include <GLTF2Importer>
 #include <Skybox>
 #include <QScreen>
+#include <MeshInstantiator>
 // Qt3D
 #include <Qt3DCore/QEntity>
 #include <Qt3DCore/QTransform>
@@ -101,7 +102,8 @@ public:
     static constexpr int Ducks = 2000;
     static constexpr int r = 2000;
 
-    Window()
+    explicit Window(const bool usesInstancing)
+        : m_usesInstancing(usesInstancing)
     {
         // Default set-up - here we create an empty entity,
         // which will contain one scene per glTF object.
@@ -127,6 +129,8 @@ public:
         fg->setExposure(1.f);
         fg->setClearColor("white");
         fg->setSkinning(true);
+        // We disable frustum culling if using instancing
+        fg->setFrustumCulling(!usesInstancing);
         setActiveFrameGraph(fg);
         //! [0.3]
 
@@ -167,37 +171,69 @@ private:
     {
         if (status == Kuesa::GLTF2Importer::Ready) {
             //! [1]
-            //! [1.1]
-            // First let's take the components that we are going to use to create our clones
-            // Gotten from gammaray
-            auto parent = m_scene->entity("KuesaEntity_0");
-            //! [1.1]
 
-            //! [1.2]
-            auto *orig_entity = qobject_cast<Qt3DCore::QEntity *>(m_scene->entity("KuesaEntity_2")->childNodes()[1]);
-            auto *orig_geometry = componentFromEntity<Qt3DRender::QGeometryRenderer>(orig_entity);
-            auto *orig_material = componentFromEntity<Qt3DRender::QMaterial>(orig_entity);
-            //! [1.2]
+            if (!m_usesInstancing) {
+                //! [1.1]
+                // First let's take the components that we are going to use to create our clones
+                // Gotten from gammaray
+                auto parent = m_scene->entity("KuesaEntity_0");
+                //! [1.1]
 
-            //! [1.3]
-            // Then create clones by giving them a custom transform, and the same components than before
-            for (int i = 0; i < Ducks; i++) {
-                auto new_entity = new Qt3DCore::QEntity{ parent };
-                auto new_transform = new Qt3DCore::QTransform;
-                new_transform->setScale(0.2f);
-                new_transform->setTranslation(QVector3D(rand() % r - r / 2, rand() % r - r / 2, rand() % r - r / 2));
+                //! [1.2]
+                auto *orig_entity = qobject_cast<Qt3DCore::QEntity *>(m_scene->entity("KuesaEntity_2")->childNodes()[1]);
+                auto *orig_geometry = componentFromEntity<Qt3DRender::QGeometryRenderer>(orig_entity);
+                auto *orig_material = componentFromEntity<Qt3DRender::QMaterial>(orig_entity);
+                //! [1.2]
 
-                new_transform->setRotationX(rand() % 360);
-                new_transform->setRotationY(rand() % 360);
-                new_transform->setRotationZ(rand() % 360);
+                //! [1.3]
+                // Then create clones by giving them a custom transform, and the same components than before
 
-                new_entity->addComponent(new_transform);
-                new_entity->addComponent(orig_geometry);
-                new_entity->addComponent(orig_material);
+                for (int i = 0; i < Ducks; i++) {
+                    auto new_entity = new Qt3DCore::QEntity{ parent };
+                    auto new_transform = new Qt3DCore::QTransform;
+                    new_transform->setScale(0.2f);
+                    new_transform->setTranslation(QVector3D(rand() % r - r / 2, rand() % r - r / 2, rand() % r - r / 2));
 
-                m_transforms[i] = new_transform;
+                    new_transform->setRotationX(rand() % 360);
+                    new_transform->setRotationY(rand() % 360);
+                    new_transform->setRotationZ(rand() % 360);
+
+                    new_entity->addComponent(new_transform);
+                    new_entity->addComponent(orig_geometry);
+                    new_entity->addComponent(orig_material);
+
+                    m_transforms[i] = new_transform;
+                }
+                //! [1.3]
+
+            } else {
+                //! [1.4]
+                // Build base transformation matrices for each instance
+                m_matrices.reserve(Ducks + 1);
+                for (int i = 0; i < Ducks; i++) {
+                    QMatrix4x4 m;
+
+                    const int extent = r / 20;
+                    m.translate(QVector3D(rand() % extent - extent * 0.5, rand() % extent - extent * 0.5, rand() % extent - extent * 0.5));
+                    m.rotate(rand() % 360, QVector3D(1.0f, 0.0f, 0.0f));
+                    m.rotate(rand() % 360, QVector3D(0.0f, 1.0f, 0.0f));
+                    m.rotate(rand() % 360, QVector3D(0.0f, 0.0f, 1.0f));
+
+                    m_matrices.push_back(m);
+                }
+                // Add an extra matrices to have one instance placed at the origin
+                m_matrices.push_back({});
+                //! [1.4]
+
+                //! [1.5]
+                // Create MeshInstantiator
+                m_meshInstantiator = new Kuesa::MeshInstantiator(m_scene);
+                // Specify which Entity with a Mesh needs to be instanced
+                m_meshInstantiator->setEntityName(QStringLiteral("KuesaEntity_2"));
+                // Set transformation matrices
+                m_meshInstantiator->setTransformationMatrices(m_matrices);
+                //! [1.5]
             }
-            //! [1.3]
 
             // Animate everyone
             qreal ms = 1000. / this->screen()->refreshRate();
@@ -209,16 +245,38 @@ private:
     void timerEvent(QTimerEvent *event) override
     {
         Q_UNUSED(event)
-        for (auto transform : m_transforms) {
-            transform->setRotationX(transform->rotationX() + 0.1f);
-            transform->setRotationY(transform->rotationY() + 0.1f);
-            transform->setRotationZ(transform->rotationZ() + 0.1f);
+        if (!m_usesInstancing) {
+            for (auto transform : m_transforms) {
+                transform->setRotationX(transform->rotationX() + 0.1f);
+                transform->setRotationY(transform->rotationY() + 0.1f);
+                transform->setRotationZ(transform->rotationZ() + 0.1f);
+            }
+        } else {
+            static bool wasInitialized = false;
+            static QMatrix4x4 rotationIncrementMatrix;
+
+            if (!wasInitialized) {
+                rotationIncrementMatrix.rotate(0.1f, QVector3D(1.0f, 0.0f, 0.0f));
+                rotationIncrementMatrix.rotate(0.1f, QVector3D(0.0f, 1.0f, 0.0f));
+                rotationIncrementMatrix.rotate(0.1f, QVector3D(0.0f, 0.0f, 1.0f));
+                wasInitialized = true;
+            }
+
+            // Apply rotation transform to all matrices
+            // This accumulates over time
+            for (QMatrix4x4 &m : m_matrices)
+                m *= rotationIncrementMatrix;
+
+            m_meshInstantiator->setTransformationMatrices(m_matrices);
         }
     }
     //! [2]
 
+    const bool m_usesInstancing;
     Kuesa::SceneEntity *m_scene{};
     std::array<Qt3DCore::QTransform *, Ducks> m_transforms;
+    std::vector<QMatrix4x4> m_matrices;
+    Kuesa::MeshInstantiator *m_meshInstantiator = nullptr;
 };
 
 int main(int argc, char *argv[])
@@ -239,7 +297,9 @@ int main(int argc, char *argv[])
     }
 
     QGuiApplication app(argc, argv);
-    Window window;
+
+    constexpr bool useInstancing = true;
+    Window window(useInstancing);
 
     window.show();
 
